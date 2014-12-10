e05353264bff2d71f59d08185823483d059c9564
CandiceSG <candice.salagasperini@gmail.com>
2014-12-10 17:16:09 +0100

    relations has many

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ActiveRecord::Base
   has_many :experiences
   has_many :user_symptoms
   has_many :symptoms, through: :user_symptoms
-
+  has_many :user_medications
+  has_many :medications, through: :user_medications
 end
