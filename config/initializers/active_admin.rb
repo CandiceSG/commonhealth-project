7d879fdca90c95eaf0997ccca1257ad3dd5a6c7e
CandiceSG <candice.salagasperini@gmail.com>
2014-12-11 21:46:48 +0100

    allow user to be admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -10,14 +10,6 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Commonhealth Project"
 
-  config.authentication_method = :authenticate_admin!
-
-  config.current_user_method = :current_user
-
-  config.logout_link_path = :destroy_user_session_path
-
-  config.logout_link_method = :delete
-
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
@@ -58,6 +50,7 @@ ActiveAdmin.setup do |config|
   # This will ONLY change the title for the admin section. Other
   # namespaces will continue to use the main "site_title" configuration.
 
+  config.authentication_method = :authenticate_admin!
   # == User Authentication
   #
   # Active Admin will automatically call an authentication
@@ -98,7 +91,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
-  config.current_user_method = :current_admin_user
+  config.current_user_method = :current_user
 
   # == Logging Out
   #
@@ -110,13 +103,13 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :destroy_admin_user_session_path
+  config.logout_link_path = :destroy_user_session_path
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :delete
 
   # == Root
   #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211203553) do
+ActiveRecord::Schema.define(version: 20141211204628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(version: 20141211203553) do
     t.string   "zipcode"
     t.string   "mood"
     t.string   "picture"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
