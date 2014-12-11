e0e5de6ec4348535edd71d3470c0b117fa45a714
CandiceSG <candice.salagasperini@gmail.com>
2014-12-11 22:35:00 +0100

    generate active medication

--- app/admin/medication.rb
+++ app/admin/medication.rb
@@ -1,5 +1,5 @@
-ActiveAdmin.register Symptom do
-   permit_params :description
+ActiveAdmin.register Medication do
+
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
