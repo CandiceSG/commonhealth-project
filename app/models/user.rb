3d052da139789c7fc500f42e6a3bfcda83141ea4
CandiceSG <candice.salagasperini@gmail.com>
2014-12-16 19:02:17 +0100

    méthode friendship dans modele user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,14 @@ class User < ActiveRecord::Base
   has_many :user_wellbeings
   has_many :wellbeings, through: :user_wellbeings
 
-  validates :first_name, :email, :password, presence: true
+  #validates :first_name, :email, :password, presence: true
 
 
-def friends(only_pending = false)
+  def friends(only_pending = false)
     user_friends = []
     my_friends = Friendship.includes([:sender, :receiver]).where("sender_id = ? or receiver_id = ?",  self.id, self.id)
-    my_friends.each do |friend|
-      if (only_pending && !friend.status) || (!only_pending && friend.status)
+    my_friends.each do |friendship|
+      if (only_pending && !friendship.status == accepted ) || (!only_pending && friendship.status == accepted)
         if friendship.receiver_id == self.id
           user_friends << friendship.sender
         else
@@ -37,4 +37,26 @@ def friends(only_pending = false)
     user_friends
   end
 
+  def friends_by_status()
+  user_friends = []
+  user_requests = []
+  user_propositions = []
+  my_friends = Friendship.includes([:sender, :receiver]).where("sender_id = ? or receiver_id = ?",  self.id, self.id)
+  my_friends.each do |friendship|
+    if friendship.status == accepted
+      if friendship.receiver_id == self.id
+        user_friends << {friendship_user: friendship.sender, friendship_relation:friendship}
+      else
+        user_friends << {friendship_user: friendship.receiver, friendship_relation:friendship}
+      end
+    else
+      if friendship.receiver_id == self.id
+        user_propositions << {friendship_user: friendship.sender, friendship_relation:friendship}
+      else
+        user_requests << {friendship_user: friendship.receiver, friendship_relation:friendship}
+      end
+    end
+  end
+  {user_friend: user_friends, user_propositions: user_propositions, user_requests: user_requests}
+end
 end
