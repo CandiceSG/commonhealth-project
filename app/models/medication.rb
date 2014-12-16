fe1ab18540cdce074ab61771ff207b58e5e29cf1
Sebastien Saunier <seb@saunier.me>
2014-12-16 03:02:50 +0100

    No need for inverse of

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,5 +1,5 @@
 class Medication < ActiveRecord::Base
-  has_many :user_medications, inverse_of: :medication
+  has_many :user_medications
   has_many :users, through: :user_medications
 
   default_scope { order('description ASC') }
