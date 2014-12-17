b70bc5aa8df5bfa44175c5e6354b0829b2b8abc3
CandiceSG <candice.salagasperini@gmail.com>
2014-12-17 15:18:21 +0100

    ajout relation friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,23 @@
 class FriendshipsController < InheritedResources::Base
 
+  def create
+    @friendship = current_user.friendship.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Added friend."
+      redirect_to root_url
+    else
+      flash[:error] = "Unable to add friend."
+      redirect_to root_url
+    end
+  end
+
+    def destroy
+      @friendship = Frienship.find(params[:id])
+      @friendship.destroy
+      flash[:notice] = "Successfully destroyed frienship."
+      redirect_to root_url
+    end
+
   private
 
     def friendship_params
