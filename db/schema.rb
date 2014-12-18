d6c254fc07ba0921673abe123fe66fb0c5f39b32
CandiceSG <candice.salagasperini@gmail.com>
2014-12-18 23:17:58 +0100

    generate model Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217164630) do
+ActiveRecord::Schema.define(version: 20141218144934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,16 @@ ActiveRecord::Schema.define(version: 20141217164630) do
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "posts", force: true do |t|
+    t.text     "content"
+    t.string   "privacy"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "supplements", force: true do |t|
     t.string   "description"
